3d08470fc1bce31e8142181588754e3cfba76bf2
kstrzelecki <strzelecki@nonstop.co>
2017-05-04 21:10:18 +0200

    comments added, cleanup :)

--- node.js
+++ node.js
@@ -1,14 +1,18 @@
+//libraries for data exhange inside node js server
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+//bebop drone client libraries
 var bebop = require('node-bebop');
 var drone = bebop.createClient();
+//library for file stream (i/o)
 var fs = require('fs');
 
-var flightPlanCoordinates = [];
+//latlng coordinates of a single drone fly
+var latLngCoordinates = [];
 
 var saveJSON = function(){
-  var j = {"latlng":flightPlanCoordinates};
+  var j = {"latlng":latLngCoordinates};
   console.log(j);
   var jsonObject = JSON.stringify(j);
   console.log(jsonObject);
@@ -19,31 +23,33 @@ var saveJSON = function(){
   });
 }
 
-//drone controller page declaration
+/**SERVER HTTP API**/
+//drone page declaration
 app.get('/drone', function(req, res){
   res.sendfile('client.html');
 });
 
-//declaration of jquery
+//declaration of jquery library
 app.get('/jquery.js', function(req, res){
   res.sendfile('jquery.js');
 });
 
+//set of js functions for handling and displaing data on client.html page
 app.get('/bebop.js', function(req, res){
   res.sendfile('bebop.js');
 });
 
-
+//page which uses latlng.json file (stored in server root dir) to present fly path on google maps
 app.get('/maps', function(req, res){
   res.sendfile('maps.html');
 });
 
 
-
+//when drone is connected this callback funct is called.
 drone.connect(function() {  
   drone.GPSSettings.resetHome();
-drone.WifiSettings.outdoorSetting(1);
- console.log("connected to drone", drone.Settings.GPSSettings);
+  drone.WifiSettings.outdoorSetting(1);
+  console.log("connected to drone", drone.Settings.GPSSettings);
 
    
 });
@@ -55,38 +61,36 @@ drone.on("battery", function(data) {
     );
 });
 
-/*drone.on("GPSFixStateChanged", function(data) {
-    console.log("GPSFixStateChanged", data);
-    io.emit('gps', data.toString());
-});*/
 
+//listener of gps change
 drone.on("PositionChanged", function(data) {
-  //data.latitude  data.longitude
+  //if data is different than 500 means it reads gps signal properly
   if(data.latitude !== 500 && data.longitude !== 500){
-    flightPlanCoordinates.push({lat: data.latitude, lng: data.longitude});
+    //new gps data is pushed to array
+    latLngCoordinates.push({lat: data.latitude, lng: data.longitude});
   }
-  console.log(flightPlanCoordinates);
-    console.log(data);
-    io.emit('gps', data);
+  console.log(latLngCoordinates);
+  //emmiting key-value to html pages
+  io.emit('gps', data);
 });
 
-//when connection with socket
+//when connection with socket appears (when someone opens /bebop or /maps page)
 io.on('connection', function(socket){
   console.log('io connection established');
 
-
+  //Reading json file with coordinates to show on map
   fs.readFile('latlng.json', 'utf8', function readFileCallback(err, data){
     if (err){
         console.log(err);
     } else {
     obj = JSON.parse(data); //now it an object
-    console.log(obj);
+    console.log("json loaded");
     io.emit('latLng', obj.latlng);
     
     }
   });
   
-  
+  //when sockets gets such message from client site it triggers proper actions in drone
   socket.on('takeOff', function(msg){
     console.log('takeOff');
     drone.takeOff();
@@ -96,6 +100,7 @@ io.on('connection', function(socket){
   socket.on('landing', function(msg){
     console.log('landing');
     drone.land();
+    //Json with data is saved after landing button is pressed
     saveJSON();
 
   });
@@ -168,10 +173,7 @@ io.on('connection', function(socket){
 
 });
 
-
-
-
-
+//http server is set to port 3000
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
